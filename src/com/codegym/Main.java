27ab35c7ddc0329afc489a92c9b5066083074df5
lmt6498 <lmt6498@users.noreply.github.com>
2021-06-11 13:37:35 +0700

    Nop bai tap

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,11 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.setName("Kaori");
+        student1.setClasses("11a2");
+        Student student2= new Student();
+    }
+}
